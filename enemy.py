8007c62d2ae4e26280fb96fbc6c24add56dab513
Amy <stargod0707@gmail.com>
2026-01-12 00:33:57 +0800

    feat: add dynamic difficulty scaling for enemies

--- enemy.py
+++ enemy.py
@@ -2,13 +2,16 @@ import pygame
 import random
 
 class EnemyPlugin:
-    def __init__(self):
+    def init(self):
         self.enemies = [[random.randint(0, 750), 0]]
         self.color = (255, 0, 0)
+        self.speed = 3 # 初始速度
 
     def update(self):
+        # 難度演進：每 500 幀稍微增加速度
+        self.speed += 0.001 
         for e in self.enemies:
-            e[1] += 3  # 往下掉
+            e[1] += self.speed
             if e[1] > 600:
                 e[0], e[1] = random.randint(0, 750), 0
 
