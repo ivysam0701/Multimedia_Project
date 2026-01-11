2cb79d7dd1f4231889ea9c9b9090209766d3a24d
Amy <stargod0707@gmail.com>
2026-01-12 00:56:41 +0800

    fix enemy error

--- enemy.py
+++ enemy.py
@@ -3,12 +3,12 @@ import random
 
 class EnemyPlugin:
     def init(self):
-        self.enemies = [[random.randint(0, 750), 0]]
+        self.enemies = [[random.randint(0, 750), 0]] # 解決 image_3dd92d.png 報錯
         self.color = (255, 0, 0)
-        self.speed = 3  # <--- 務必加上這行，解決 image_3dd228.png 的錯誤
+        self.speed = 3 # 解決 image_3dd228.png 報錯
 
     def update(self):
-        # 這裡會用到 self.speed
+        self.speed += 0.001
         for e in self.enemies:
             e[1] += self.speed
             if e[1] > 600:
