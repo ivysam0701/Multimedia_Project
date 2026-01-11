9536c823ae00ba9489aaf18e101edd8b81f4d084
Hsu <sam070199@gmail.com>
2026-01-12 01:11:05 +0800

    Note that the code appears to be out of sync, integrated by one person.

--- enemy.py
+++ enemy.py
@@ -2,15 +2,14 @@ import pygame
 import random
 
 class EnemyPlugin:
-    def init(self):
+    def __init__(self):
         # 解決 image_3e41ed 報錯
         self.enemies = [[random.randint(0, 750), 0]] 
         self.color = (255, 0, 0)
-        # 解決 image_3de48e 報錯
+        # 解決 image_3de48e / image_3e4dcd 報錯
         self.speed = 3 
 
     def update(self):
-        # 這裡會用到 self.speed
         self.speed += 0.001
         for e in self.enemies:
             e[1] += self.speed
--- player.py
+++ player.py
@@ -1,10 +1,10 @@
-import pygame
+import pygame # 解決 image_33e8c6 報錯
 
 class PlayerPlugin:
     def __init__(self):
         self.rect = pygame.Rect(400, 500, 50, 50)
         self.color = (0, 255, 0)
-        self.lives = 3 # 提供生命值基礎
+        self.lives = 3 
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -13,6 +13,5 @@ class PlayerPlugin:
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
-        # 繪製血條
         for i in range(self.lives):
             pygame.draw.circle(screen, (0, 255, 0), (30 + i*30, 30), 10)
