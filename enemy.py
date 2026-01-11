fe52c5c9cd9faaedc0ef1957bca925c5f68366c1
Amy <stargod0707@gmail.com>
2026-01-12 01:07:55 +0800

    fix

--- enemy.py
+++ enemy.py
@@ -3,11 +3,14 @@ import random
 
 class EnemyPlugin:
     def init(self):
-        self.enemies = [[random.randint(0, 750), 0]] # 解決 enemies 缺失
+        # 解決 image_3e41ed 報錯
+        self.enemies = [[random.randint(0, 750), 0]] 
         self.color = (255, 0, 0)
-        self.speed = 3 # 解決 speed 缺失
+        # 解決 image_3de48e 報錯
+        self.speed = 3 
 
     def update(self):
+        # 這裡會用到 self.speed
         self.speed += 0.001
         for e in self.enemies:
             e[1] += self.speed
